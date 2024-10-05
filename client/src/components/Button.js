a70c0f92736f25d6c9f6da002c1d71bdd8166cfc
Kevin Neave <kevinneave@Kevins-MacBook-Air.local>
2024-10-05 18:10:04 -0400

    made login and signup card

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -4,7 +4,7 @@ const Button = ({ href, label }) => {
   return (
     <a
       href={href}
-      className="px-6 py-3 text-sm text-white bg-gray-800 rounded-full hover:bg-gray-600 transition"
+      className="px-6 py-3 text-sm text-white bg-gray-800 rounded-full hover:bg-gray-600 hover:cursor-pointer transition"
       style={{ display: 'inline-block', width: 'auto' }} // Ensure it's inline with auto width
     >
       {label}
--- client/src/sections/ContactSection.js
+++ client/src/sections/ContactSection.js
@@ -3,7 +3,7 @@ import ContactForm from '../components/ContactForm';
 
 const ContactSection = () => {
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center bg-gray-50">
+    <div className="w-full h-screen flex flex-col items-center justify-center bg-gray-100">
       //Title for page
       <h2 className="text-4xl font-light text-gray-800 mb-8">About Us</h2>
       //Calls contact form from components
